d8f8bc1431567debf8ab85cb143935b9045091b4
Raul Garcia <rauld.garcia95@gmail.com>
2022-10-30 17:55:10 -0600

    imprime lista de libros ordenados de mayor a menor

--- minigoogle.py
+++ minigoogle.py
@@ -1,3 +1,18 @@
+import operator
+import numpy as np
+
+libros=["Analisis de Algoritmos.txt",
+    "Artificial Intelligence A Modern Approach.txt",
+    "Introduction to algorithms.txt",
+    "Introduction to the Design and Analysis of Algorithms.txt",
+    "Introduction To The Theory Of Computation.txt",
+    "Pattern Recognition and Machine Learning .txt",
+    "Probability and Computing Randomization and Probabilistic Techniques in Algorithms and Data Analysis.txt",
+    "PROLOG.txt",
+    "Remote Sensing Digital Image Analysis.txt",
+    "Theory of Computation.txt"]
+contlibros=[0,0,0,0,0,0,0,0,0,0]
+
 def construyeg(b): # construye un diccionario para representar la función g
     d={}
     for i in range(0,len(b)-1): # ignoramos el último caracter
@@ -20,6 +35,8 @@ def bmh(a,b): # busca b dentro de a, retorna None (fracaso) o posición del calc
             j=m-1
     return None
 
+
+#BUSQUEDA DE PALABRA EN ARCHIVO ESPECIFICO
 palabra=input('Ingrese la palabra que desea buscar:')
 print(palabra)
 
@@ -34,5 +51,29 @@ with open("Analisis de Algoritmos.txt") as archivo:
             print("La palabra se encuentra en el caracter:",busqueda)
             cont=cont+1
 
-print("Se encontro la palabra:")
+print("El numero de veces que se encontro la palabra es:")
 print(cont)
+
+#BUSQUEDA DE PALABRA EN TODA LA CARPETA Y ORDENAMIENTO
+palabra=input('Ingrese la palabra que desea buscar:')
+print(palabra)
+
+for i in range(10):
+
+    cont=0
+    nlinea=0
+    libro=libros[i]
+    with open(libro) as archivo:
+        for linea in archivo:
+            nlinea=nlinea+1
+            busqueda=bmh(linea,palabra)
+            if busqueda != None:
+                cont=cont+1
+
+    contlibros[i]=cont
+
+dic=dict(zip(libros,contlibros))
+
+ord=(sorted(dic.items(),key=operator.itemgetter(1),reverse=True))
+for i in ord:
+    print(i)
